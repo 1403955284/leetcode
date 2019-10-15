f7bf465bcd9fdeca3c3ce33d39c28f28f7fde1bb
miyagi <1403955284@qq.com>
2019-10-15 14:48:55 +0800

    add

---
+++ 130.surrounded-regions.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode id=130 lang=java
+ *
+ * [130] Surrounded Regions
+ */
+
+// @lc code=start
+class Solution {
+    public void solve(char[][] board) {
+        if(board==null||board.length==0||board[0].length==0){
+            return;
+        }
+        for(int i = 0;i<board[0].length;i++){
+            helper(board, 0, i);
+            helper(board, board.length-1, i);
+        }
+        for(int i = 0;i<board.length;i++){
+            helper(board, i, 0);
+            helper(board, i, board[0].length-1);
+        }
+        for(int i = 0;i<board.length;i++){
+            for(int j = 0;j<board[0].length;j++){
+                if(board[i][j]=='O'){
+                    board[i][j] ='X';
+                }
+                if(board[i][j]=='#'){
+                    board[i][j]='O';
+                }
+            }
+        }
+    }
+
+    public void helper(char[][] board,int i,int j){
+        if(board[i][j]!='O'){
+            return;
+        }
+        board[i][j] = '#';
+        LinkedList<Integer[]> queue = new LinkedList<>();
+        Integer[] init = new Integer[]{i,j};
+        queue.addLast(init);
+        while(!queue.isEmpty()){
+            Integer[] cur = queue.pollFirst();
+            int col = cur[0];
+            int raw = cur[1];
+            if(col>0&&board[col-1][raw]=='O'){
+                board[col-1][raw] = '#';
+                queue.addLast(new Integer[]{col-1,raw});
+            }
+            if(col<board.length-1&&board[col+1][raw]=='O'){
+                board[col+1][raw] = '#';
+                queue.addLast(new Integer[]{col+1,raw});
+            }
+            if(raw<board[0].length-1&&board[col][raw+1]=='O'){
+                board[col][raw+1] = '#';
+                queue.addLast(new Integer[]{col,raw+1});
+            }
+            if(raw>0&&board[col][raw-1]=='O'){
+                board[col][raw-1] = '#';
+                queue.addLast(new Integer[]{col,raw-1});
+            }
+        }
+    }
+    
+
+}
+// @lc code=end
+
