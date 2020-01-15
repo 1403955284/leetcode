50fb54f0bacb10b7cbcd4c5ea868c87d626e16d3
1403955284 <1403955284@qq.com>
2020-01-15 17:03:01 +0800

    add

--- 403.frog-jump.java
+++ 403.frog-jump.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 /*
  * @lc app=leetcode id=403 lang=java
@@ -13,11 +11,22 @@ import java.util.Map;
 // @lc code=start
 class Solution {
     public boolean canCross(int[] stones) {
-        boolean[][] dp = new boolean[stones.length][stones.length];
-        dp[0][0] = true;
-        for(int cur:stones){
-            
+        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        for (int i = 0; i < stones.length; i++) {
+            map.put(stones[i], new HashSet<Integer>());
         }
+        map.get(0).add(0);
+        for (int i = 0; i < stones.length; i++) {
+            for (int k : map.get(stones[i])) {
+                for (int step = k - 1; step <= k + 1; step++) {
+                    if (step > 0 && map.containsKey(stones[i] + step)) {
+                        map.get(stones[i] + step).add(step);
+                    }
+                }
+            }
+        }
+        return map.get(stones[stones.length - 1]).size() > 0;
+
     }
 
 
